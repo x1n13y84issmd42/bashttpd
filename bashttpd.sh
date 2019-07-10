45cd10f93605eefc3b76c9048238c41b20d74e4e
Eugene Merkulov <gaddeena@gmail.com>
2019-07-10 05:40:59 +0300

    It's in a loop now, like a real server

--- bashttpd.sh
+++ bashttpd.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Bashttpd 0.1"
+echo "Bashttpd 0.2"
 echo "Project: $1"
 
-netcat -lvk -p 8080 -c "./handler.sh $1"
+while true; do
+	netcat -lvk -p 8080 -c "./handler.sh $1"
+
+	if [ $? -gt 0 ]; then
+		echo "It was nice having you here. Come back one day."
+		break;
+	fi
+done
