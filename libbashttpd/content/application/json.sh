abcecd1ec0418235f40cab0ab0b171d2a2086bba
Eugene Merkulov <gaddeena@gmail.com>
2019-08-04 14:25:58 +0300

    Figured out JSON requesst

--- libbashttpd/content/application/json.sh
+++ libbashttpd/content/application/json.sh
@@ -8,16 +8,10 @@ if [ -z ${CONTENT_LENGTH+x} ]; then
 	:
 else
 	while [ $CL -lt $CONTENT_LENGTH ]; do
-		read -n1 CHAR
-		# read CHAR
+		read -n1 -r CHAR
 		BODY="$BODY$CHAR"
-		# printf "%s" $CHAR | tr -d "\r" >&2
 		let CL=CL+1
-		# log "$CL - '$CHAR'"
-		# printf "%q" "${CHAR}\n" >&2
 	done;
-
-	log "DONE------"
 
 	# Debug dump
 	[[ ! -z $DEBUG_DUMP_BODY ]] && echo -n "$BODY" > $DEBUG_DUMP_BODY
@@ -25,10 +19,7 @@ fi
 
 # An implementation of reqData.
 function reqDataImpl {
-	log "The JSON body is"
-	log ""
-	log $BODY
-	yield $(echo $BODY | jq -r $1)
+	yield $(echo -nE "$BODY" | jq -r $1)
 }
 
 IFS=$_IFS
