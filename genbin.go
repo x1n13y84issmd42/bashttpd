830930eb773ae1d7b63ae14a20c717f516b8a3cd
Eugene Merkulov <gaddeena@gmail.com>
2019-08-06 19:40:03 +0300

    Added an utility to generate binary payload for test requests

---
+++ genbin.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	bytes15 := []byte("\n\n\n\x00\x00\x00\xaa\xbb\xcc\r\r\r\x00\xde\xad")
+	bytes20 := []byte("\n\n\n\n\x00\x00\x00\x00\xaa\xbb\xcc\xdd\r\r\r\r\xde\xad\xbe\xef")
+	bytesABCD := []byte("ABCD\n\n\n\nAB\rD\x00\x00\x00\x00\xde\xad\xbe\xef")
+
+	var err error
+	err = ioutil.WriteFile("./testdata/15.bin", bytes15, 0777)
+	err = ioutil.WriteFile("./testdata/20.bin", bytes20, 0777)
+	err = ioutil.WriteFile("./testdata/ABCD.bin", bytesABCD, 0777)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	fmt.Println("Yolo world")
+}
