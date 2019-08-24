872b2e46fe68c61a366178b8e18ecbb56da67169
Eugene Merkulov <gaddeena@gmail.com>
2019-08-24 09:32:50 +0300

    More CLI colors

--- example.com/api/ls/GET.sh
+++ example.com/api/ls/GET.sh
@@ -1,7 +1,8 @@
 respStatus 200
 
 COMMAND="ls -la --color=always"
-coloredOutput=($(ls -la --color=always))
+COMMAND="ls -la --color=always ~"
+coloredOutput=($(ls -la --color=always ~))
 CLIOUTPUT=$(bwf.respCLI ${coloredOutput[@]})
 
 respTemplateFile "assets/tpl/ls.html"
